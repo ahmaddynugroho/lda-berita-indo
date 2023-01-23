0464a99ae5a6856d6c6d9496264b44fe28343a22
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-01-23 11:49:11 +0700

    chore: autopep8

--- scrape/spiders/__init__.py
+++ scrape/spiders/__init__.py
@@ -4,20 +4,20 @@
 # your spiders.
 import scrapy
 
+
 class Headlines(scrapy.Spider):
     name = 'headlines'
     start_urls = ['https://indeks.kompas.com/?site=all&date=2023-01-01&page=1']
     # start_urls = [f'https://indeks.kompas.com/?site=all&date=2022-12-{day:02}&page=1' for day in range (1, 32)]
-    
+
     def parse(self, response):
         headlines = response.css('a.article__link::text').getall()
         headlines_date = response.css('div.article__date::text').getall()
         for headline, date in zip(headlines, headlines_date):
-            yield {
-                'headline': headline,
-                'date': date.split(',')[0]
-            }
-            
+            yield {'headline': headline,
+                   'date': date.split(',')[0]
+                   }
+
         next_links = response.css('a.paging__link--next')
         for a in next_links:
             if 'Next' in a.get():
